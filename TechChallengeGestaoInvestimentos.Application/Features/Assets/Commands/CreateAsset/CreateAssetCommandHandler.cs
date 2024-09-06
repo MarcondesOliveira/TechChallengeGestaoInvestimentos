8ebe7740030598166004fec41d99af6c9d7d50d7
MarcondesOliveira <marcondescurso@outlook.com>
2024-09-06 00:17:09 -0300

    first

---
+++ TechChallengeGestaoInvestimentos.Application/Features/Assets/Commands/CreateAsset/CreateAssetCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TechChallengeGestaoInvestimentos.Domain.Entities;
+using TechChallengeGestaoInvestimentos.Domain.Enum;
+using TechChallengeGestaoInvestimentos.Domain.Interfaces.Persistence;
+using Transaction = TechChallengeGestaoInvestimentos.Domain.Entities.Transaction;
+
+namespace TechChallengeGestaoInvestimentos.Application.Features.Assets.Commands.CreateAsset
+{
+    public class CreateAssetCommandHandler : IRequestHandler<CreateAssetCommand, Guid>
+    {
+        private readonly IAsyncRepository<Asset> _assetRepository;
+        private readonly IAsyncRepository<Portfolio> _portfolioRepository;
+        private readonly IAsyncRepository<Transaction> _transactionRepository;
+        private readonly IMapper _mapper;
+
+        public CreateAssetCommandHandler(IAsyncRepository<Asset> assetRepository, IAsyncRepository<Transaction> transactionRepository, IMapper mapper, IAsyncRepository<Portfolio> portfolioRepository)
+        {
+            _assetRepository = assetRepository;
+            _portfolioRepository = portfolioRepository;
+            _transactionRepository = transactionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateAssetCommand request, CancellationToken cancellationToken)
+        {
+            await new CreateAssetCommandValidator(_portfolioRepository).ValidateAndThrowAsync(request, cancellationToken);
+
+            // Definir o preço inicial com base no tipo de ativo
+            decimal initialPrice = request.AssetType switch
+            {
+                AssetType.Stocks => 100.00m,
+                AssetType.Bonds => 1000.00m,
+                AssetType.Cryptocurrencies => 332635.00m,
+                _ => throw new InvalidOperationException("Tipo de ativo inválido.")
+            };
+
+            // Criar o asset
+            var asset = _mapper.Map<Asset>(request);
+            asset.AssetId = Guid.NewGuid(); // Criar o ID do asset
+            asset.Status = "A"; // Definir o status como 'A' ao criar
+
+            await _assetRepository.AddAsync(asset);
+
+            // Criar a transação de compra automaticamente
+            var transaction = new Transaction
+            {
+                Id = Guid.NewGuid(),
+                AssetId = asset.AssetId,
+                PortfolioId = request.PortfolioId,
+                TransactionType = TransactionType.Buy,
+                Quantity = 1, // Quantidade padrão, pode ser ajustada
+                Price = initialPrice,
+                TransactionDate = DateTime.UtcNow
+            };
+
+            await _transactionRepository.AddAsync(transaction);
+
+            return asset.AssetId;
+        }
+    }
+}
---
+++ TechChallengeGestaoInvestimentos.Persistence/Migrations/20240906030600_firstMigration.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechChallengeGestaoInvestimentos.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    PortfolioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Description = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Status = table.Column<string>(type: "varchar(1)", nullable: false, defaultValue: "A")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.PortfolioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Assets",
+                columns: table => new
+                {
+                    AssetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssetType = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Code = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Status = table.Column<string>(type: "varchar(1)", nullable: false, defaultValue: "A"),
+                    PortfolioId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assets", x => x.AssetId);
+                    table.ForeignKey(
+                        name: "FK_Assets_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "PortfolioId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortfolioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactionType = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "PortfolioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_PortfolioId",
+                table: "Assets",
+                column: "PortfolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_PortfolioId",
+                table: "Transactions",
+                column: "PortfolioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+        }
+    }
+}
